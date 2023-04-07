043f85653d88f0f5abb394aa19e8fd669485f1b9
Enes Polat <turksancaktar1071@gmail.com>
2023-04-07 14:42:28 +0300

    hakkimizda ve iletisim sayfalari eklendi

--- django_firmasi/urls.py
+++ django_firmasi/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from page.views import home
+from page.views import (
+    home_view, 
+    about_us_view, 
+    contact_us_view
+    )
 
 
 
 urlpatterns = [
-    path('',home),
+    path('',home_view),
+    path('hakkimizda/',about_us_view),
+    path('iletisim/',contact_us_view),
     path("admin/", admin.site.urls),
 ]
 
--- page/views.py
+++ page/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from random import randint
 
-def home(request):
+def home_view(request):
     #print("merhaba: ",request)
     #return HttpResponse("Ana sayfaya hoş geldiniz 2022")
 	#context={"platform":f"Django patformu kullanıldı ve Randint ile dönen veri : {randint(1,100)}"}
 	context=dict()
 	return render(request,'page/home_page.html',context)
+
+def about_us_view(request):
+	context=dict()
+	return render(request,'page/about_us.html',context)
+
+def contact_us_view(request):
+	context=dict()
+	return render(request,'page/contact_us.html',context)
 
 
