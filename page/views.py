188804efadcf601c0138c07d3c3ca20d9fce5ddb
Enes Polat <turksancaktar1071@gmail.com>
2023-04-07 14:23:35 +0300

    ana sayfa eski projeden eklendi

--- page/views.py
+++ page/views.py
@@ -5,7 +5,8 @@ from random import randint
 def home(request):
     #print("merhaba: ",request)
     #return HttpResponse("Ana sayfaya hoş geldiniz 2022")
-	context={"platform":f"Django patformu kullanıldı ve Randint ile dönen veri : {randint(1,100)}"}
+	#context={"platform":f"Django patformu kullanıldı ve Randint ile dönen veri : {randint(1,100)}"}
+	context=dict()
 	return render(request,'page/home_page.html',context)
 
 
